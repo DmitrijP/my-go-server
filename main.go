89d371a5e84799698d7897a92f9e2436035aec2d
Dmitrij Patuk <dmitrij.patuk@gmx.de>
2024-10-18 00:18:23 +0200

    added index.html handler and logo.png handler as well as a logger

--- main.go
+++ main.go
@@ -2,18 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	if _, err := os.Stat("./index.html"); os.IsNotExist(err) {
+		log.Fatalf("index.html not found: %v", err)
+	}
+
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Serving %s to %s", r.URL.Path, r.RemoteAddr)
+		http.ServeFile(w, r, "./index.html")
+	})
+
+	mux.HandleFunc("/assets/logo.png", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Serving %s to %s", r.URL.Path, r.RemoteAddr)
+		http.ServeFile(w, r, "./assets/logo.png")
+	})
+
 	srv := &http.Server{
 		Handler: mux,
 		Addr:    ":8080",
 	}
 
+	fmt.Println("Starting server on :8080")
 	err := srv.ListenAndServe()
 	if err != nil {
-		fmt.Errorf("Start Error %w", err)
+		log.Fatalf("Server start error: %v", err)
 	}
 }
